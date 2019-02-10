6f65ccc48c60270bafc1f26333ead5f01555587d
Vitor Xisto <lucas.magalhaes.correa@gmail.com>
2019-02-10 14:21:54 -0200

    domingo1420

--- vrcursoWebService/src/java/ws/dao/IDao.java
+++
@@ -1,9 +0,0 @@
-package ws.dao;
-
-import ws.exception.ValidacaoException;
-
-public interface IDao {
-    
-    public void validar(Object o) throws ValidacaoException;
-    
-}
--- vrcursoWebService/src/java/ws/exception/ValidacaoException.java
+++
@@ -1,9 +0,0 @@
-package ws.exception;
-
-public class ValidacaoException extends Exception {
-
-    public ValidacaoException(String i_mensagem) {
-        super(i_mensagem);
-    }
-    
-}
